d61a57e0f6125e181bbc12c8e8a8f19a6ea2c5e0
Clarity-89 <homes89@ukr.net>
2015-09-02 14:48:02 +0300

    Add timer

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-/**
- * Created by Alex on 02/09/2015.
- */
+
+(function () {
+    var time = 1500000;
+
+    function timer() {
+        var timer = document.getElementById('timer');
+        time -= 1000;
+        timer.innerHTML = msToTime(time);
+    }
+
+    function msToTime(s) {
+
+        function addZ(n) {
+            return (n < 10 ? '0' : '') + n;
+        }
+
+        var ms = s % 1000;
+        s = (s - ms) / 1000;
+        var secs = s % 60;
+        s = (s - secs) / 60;
+        var mins = s % 60;
+        var hrs = (s - mins) / 60;
+
+        return addZ(hrs) + ':' + addZ(mins) + ':' + addZ(secs);
+    }
+
+    setInterval(timer, 1000);
+})();
