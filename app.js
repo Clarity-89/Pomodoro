0e47a03aca89e96cfa8b22af4a63f3fdd8064d54
Clarity-89 <homes89@ukr.net>
2015-09-07 19:53:25 +0300

    add sounds

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 (function ($) {
     // Default timer
-    var defaultTime = 1500000, // 25 minutes
-        pauseTime = 3000,
+    var defaultTime = 1500000 / 100, // 25 minutes
+        pauseTime = 3000, // 5 minutes for a short break
         userSetTime, //the length of  pomodoro set by user (if different from the default time)
-        t = null,
+        t = null, // a switch to check if the timer has been started
         increase = $('#increase'),
         decrease = $('#decrease'),
-        timer = document.getElementById('timer');
-    var alarm = new Audio("audio/gong2.wav");
-    var clock = new Audio('audio/clock2.mp3');
+        session = $('#session'),
+        count = 1, // number of sessions
+        audio = true,
+        timer = document.getElementById('timer'),
+        alarm = new Audio("audio/gong2.wav"),
+        clock = new Audio('audio/clock2.mp3'),
+        winding = new Audio('audio/winding2.mp3');
 
     function setTime() {
-        return timer.innerHTML = msToTime(defaultTime, false);
+        return timer.innerHTML = msToTime(defaultTime, true);
     }
 
     function runTimer() {
         defaultTime -= 1000;
         timer.innerHTML = msToTime(defaultTime, true);
-        clock.play();
+        if (audio) clock.play();
+        else clock.pause();
     }
 
     function runBreak() {
@@ -39,30 +44,43 @@
         var mins = s % 60;
         var hrs = (s - mins) / 60;
 
-        return seconds ? addZ(hrs)+ ':' + addZ(mins) + ':' + addZ(secs) : addZ(hrs) + ':' + addZ(mins);
+        return seconds ? addZ(hrs) + ':' + addZ(mins) + ':' + addZ(secs) : addZ(hrs) + ':' + addZ(mins);
     }
 
     function pomodoro() {
 
         if (defaultTime > 0) {
             runTimer();
-            if (defaultTime === 0) {
-                pauseTime = 3000;
-
-            } else {
-                alarm.pause();
+            if (defaultTime === 0 && count < 4) {
+                pauseTime = 3000 * 4;
+                if (pauseTime = 3000 * 4) {
+                    clock.pause();
+                    alarm.play();
+                }
+            } else if (defaultTime === 0 && count === 4) {
+                count = 0;
+                pauseTime = 3000 * 5;
+                clock.pause();
+                alarm.play();
             }
         } else {
+            session.text('Break!');
             clock.pause();
-            alarm.play();
+
             runBreak();
             if (pauseTime === 0) {
+                count += 1;
                 defaultTime = userSetTime;
+                alarm.play();
+                session.text('Session: ' + count);
             }
         }
     }
 
-   increase.click(function () {
+    increase.click(function () {
+        if (audio) {
+            winding.play();
+        }
         if (t === null && defaultTime < 3600000) {
             defaultTime = (defaultTime + 300000);
             setTime();
@@ -70,7 +88,10 @@
     });
 
     decrease.click(function () {
-        if (t === null && defaultTime > 1200000) {
+        if (audio) {
+            winding.play();
+        }
+        if (t === null && defaultTime > 300000) {
             defaultTime = (defaultTime - 300000);
             setTime();
         }
@@ -82,7 +103,7 @@
             increase.hide();
             decrease.hide();
             userSetTime = defaultTime;
-            console.log(userSetTime);
+            session.text('Session: ' + count);
         }
     });
 
@@ -90,12 +111,23 @@
         clearInterval(t);
         t = null;
         //set time to default value
-        defaultTime = userSetTime||defaultTime;
+        defaultTime = userSetTime || defaultTime;
         setTime();
         clock.pause();
         alarm.pause();
         increase.show();
         decrease.show();
+        count = 1;
+        session.html('&nbsp;');
+    });
+    $('#volume').click(function () {
+        if (audio) {
+            $(this).attr('class', 'fa fa-volume-off fa-3x');
+            audio = false;
+        } else {
+            $(this).attr('class', 'fa fa-volume-up fa-3x');
+            audio = true;
+        }
     });
     setTime();
 })(jQuery);
